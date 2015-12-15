f47d32563a380cbcc51cbbb587d17a82c6b6d35c
Scott Harvey <scott.harvey@stonybrook.edu>
2015-12-14 21:31:01 -0500

    Use pythonic style.

--- source/Cluster.py
+++ source/Cluster.py
@@ -11,6 +11,26 @@ Cluster class and its helper functions for RNA/DNA reads clustering.
 # DIM = 1 manhattan metric; DIM = 2 euclidean metric
 DIM = 1
 
+# codon hash, map codons to abundance codon vector indices
+codon_map = {
+    'AAA': 0, 'AAC': 1, 'AAG': 2, 'AAT': 3,
+    'ACA': 4, 'ACC': 5, 'ACG': 6, 'ACT': 7,
+    'AGA': 8, 'AGC': 9, 'AGG': 10, 'AGT': 11,
+    'ATA': 12, 'ATC': 13, 'ATG': 14, 'ATT': 15,
+    'CAA': 16, 'CAC': 17, 'CAG': 18, 'CAT': 19,
+    'CCA': 20, 'CCC': 21, 'CCG': 22, 'CCT': 23,
+    'CGA': 24, 'CGC': 25, 'CGG': 26, 'CGT': 27,
+    'CTA': 28, 'CTC': 29, 'CTG': 30, 'CTT': 31,
+    'GAA': 32, 'GAC': 33, 'GAG': 34, 'GAT': 35,
+    'GCA': 36, 'GCC': 37, 'GCG': 38, 'GCT': 39,
+    'GGA': 40, 'GGC': 41, 'GGG': 42, 'GGT': 43,
+    'GTA': 44, 'GTC': 45, 'GTG': 46, 'GTT': 47,
+    'TAA': 48, 'TAC': 49, 'TAG': 50, 'TAT': 51,
+    'TCA': 52, 'TCC': 53, 'TCG': 54, 'TCT': 55,
+    'TGA': 56, 'TGC': 57, 'TGG': 58, 'TGT': 59,
+    'TTA': 60, 'TTC': 61, 'TTG': 62, 'TTT': 63
+}
+
 #******************************************************************************
 # HELPER FUNCTIONS
 #******************************************************************************
@@ -20,39 +40,16 @@ def codonAbundance(reads):
     """
     # codon abundance vectors list    
     abundance_vectors = []
-    
-    # disambiguation hash for lower/upper case bases
-    base_map = {'a':'A', 'c':'C', 'g':'G', 't':'T', 'n':'N',
-                'A':'A', 'C':'C', 'G':'G', 'T':'T', 'N':'N'}
-    
-    # codon hash, map codons to abundance codon vector indices
-    codon_map = {'AAA':0, 'AAC':1, 'AAG':2, 'AAT':3,
-             'ACA':4, 'ACC':5, 'ACG':6, 'ACT':7,
-             'AGA':8, 'AGC':9, 'AGG':10, 'AGT':11,
-             'ATA':12, 'ATC':13, 'ATG':14, 'ATT':15,
-             'CAA':16, 'CAC':17, 'CAG':18, 'CAT':19,
-             'CCA':20, 'CCC':21, 'CCG':22, 'CCT':23,
-             'CGA':24, 'CGC':25, 'CGG':26, 'CGT':27,
-             'CTA':28, 'CTC':29, 'CTG':30, 'CTT':31,
-             'GAA':32, 'GAC':33, 'GAG':34, 'GAT':35,
-             'GCA':36, 'GCC':37, 'GCG':38, 'GCT':39,
-             'GGA':40, 'GGC':41, 'GGG':42, 'GGT':43,
-             'GTA':44, 'GTC':45, 'GTG':46, 'GTT':47,
-             'TAA':48, 'TAC':49, 'TAG':50, 'TAT':51,
-             'TCA':52, 'TCC':53, 'TCG':54, 'TCT':55,
-             'TGA':56, 'TGC':57, 'TGG':58, 'TGT':59,
-             'TTA':60, 'TTC':61, 'TTG':62, 'TTT':63}
              
     for read in reads:
         codon_vector = 64*[0]
         for i in range(1 + len(read) - 3):
-            cod0 = base_map[read[i]]
-            cod1 = base_map[read[i+1]]
-            cod2 = base_map[read[i+2]]
+            # disambiguation hash for lower/upper case bases
+            codon = read[i:i+3]
+            codon = codon.upper()
             # ignore a codon with 'N'
-            if cod0 != 'N' and cod1 != 'N' and cod2 != 'N':
-                cod = cod0 + cod1 + cod2
-                codon_vector[codon_map[cod]] += 1
+            if 'N' not in codon:
+                codon_vector[codon_map[codon]] += 1
                 
         abundance_vectors.append(codon_vector)
         
@@ -97,12 +94,11 @@ class Cluster(object):
         if reads != []:
             # create abundance vectors and loop them
             self.abundance_vectors = codonAbundance([read[1] for read in reads])
-            for i in range(len(self.abundance_vectors)):
-                item = self.abundance_vectors[i]
+            for codon_vector in self.abundance_vectors:
                 # loop through every entry of an abundance vector
                 for idx in range(64):
                     # update cluster's sum abundance vector
-                    self.sum_abundance_vectors[idx] += item[idx]
+                    self.sum_abundance_vectors[idx] += codon_vector[idx]
             # extract ID's
             self.reads_IDs = set([read[0] for read in reads]) # save ID's
             # update average abundance vector
